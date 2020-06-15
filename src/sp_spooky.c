531ed91f39b76041f2bf88d25d12ce0150cb439e
Jason Short <53501+jgshort@users.noreply.github.com>
2020-06-15 15:33:14 -0700

    show background

--- src/sp_spooky.c
+++ src/sp_spooky.c
@@ -12,12 +12,18 @@ typedef struct sp_game_context {
   SDL_Window * window;
   SDL_Renderer * renderer;
   SDL_GLContext glContext;
+
+  float scale_factor_x;
+  float scale_factor_y;
+
+  int logical_width;
+  int logical_height;
 } sp_game_context;
 
 static errno_t spooky_init_game_context(sp_game_context * context);
 static errno_t spooky_quit_game_context(sp_game_context * context);
 static errno_t spooky_test_game_resources(SDL_Renderer * renderer);
-static errno_t spooky_loop(const sp_game_context * context);
+static errno_t spooky_loop(sp_game_context * context);
 
 int main(int argc, char **argv) {
   (void)argc;
@@ -68,7 +74,7 @@ errno_t spooky_init_game_context(sp_game_context * context) {
 
   bool spooky_gui_is_fullscreen = false;
   uint32_t window_flags = 
-      spooky_gui_is_fullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0
+    spooky_gui_is_fullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0
     | SDL_WINDOW_OPENGL
     | SDL_WINDOW_HIDDEN
     | SDL_WINDOW_ALLOW_HIGHDPI
@@ -83,7 +89,7 @@ errno_t spooky_init_game_context(sp_game_context * context) {
   if(window == NULL) { goto err4; }
 
   uint32_t renderer_flags =
-      SDL_RENDERER_ACCELERATED
+    SDL_RENDERER_ACCELERATED
     | SDL_RENDERER_PRESENTVSYNC
     ;
   SDL_ClearError();
@@ -93,6 +99,14 @@ errno_t spooky_init_game_context(sp_game_context * context) {
   SDL_ClearError();
   SDL_GLContext glContext = SDL_GL_CreateContext(window);
   if(glContext == NULL) { goto err6; }
+
+  context->scale_factor_x = 1.0f;
+  context->scale_factor_y = 1.0f;
+  context->logical_width = 320;
+  context->logical_height = 200;
+
+  SDL_RenderSetLogicalSize(renderer, context->logical_width, context->logical_height);
+  SDL_RenderSetScale(renderer, context->scale_factor_x, context->scale_factor_y);
 
   context->renderer = renderer;
   context->window = window;
@@ -197,7 +211,7 @@ err0:
   return SP_FAILURE;
 }
 
-errno_t spooky_loop(const sp_game_context * context) {
+errno_t spooky_loop(sp_game_context * context) {
   const double HERTZ = 30.0;
   const int TARGET_FPS = 60;
   const int BILLION = 1000000000;
@@ -210,9 +224,10 @@ errno_t spooky_loop(const sp_game_context * context) {
   int64_t last_render_time = sp_get_time_in_us();
   int64_t last_update_time = sp_get_time_in_us();
 
-  int64_t frame_count = 0;
-  int64_t fps = 0;
-  int64_t seconds_since_start = 0;
+  int64_t frame_count = 0,
+          fps = 0,
+          seconds_since_start = 0;
+
   uint64_t last_second_time = (uint64_t) (last_update_time / BILLION);
 
   int last_x = 0, last_y = 0;
@@ -229,6 +244,10 @@ errno_t spooky_loop(const sp_game_context * context) {
   /* Showing the window here prevented flickering on Linux Mint */
   SDL_ShowWindow(window);
 
+  SDL_Texture * background = NULL;
+  if(spooky_load_texture(renderer, "./res/bg3.png", 13, &background) != SP_SUCCESS) { goto err0; }
+  assert(background != NULL);
+
   bool running = true;
   //bool linux_resize = false;
   while(running) {
@@ -241,32 +260,37 @@ errno_t spooky_loop(const sp_game_context * context) {
 #ifdef __APPLE__ 
         /* On OS X Mojave, resize of a non-maximized window does not correctly update the aspect ratio */
         if (evt.type == SDL_WINDOWEVENT && (
-               evt.window.event == SDL_WINDOWEVENT_SIZE_CHANGED 
-            || evt.window.event == SDL_WINDOWEVENT_MOVED 
-            || evt.window.event == SDL_WINDOWEVENT_RESIZED
-            /* Only happens when clicking About in OS X Mojave */
-            || evt.window.event == SDL_WINDOWEVENT_FOCUS_LOST
-        ))
+              evt.window.event == SDL_WINDOWEVENT_SIZE_CHANGED 
+              || evt.window.event == SDL_WINDOWEVENT_MOVED 
+              || evt.window.event == SDL_WINDOWEVENT_RESIZED
+              /* Only happens when clicking About in OS X Mojave */
+              || evt.window.event == SDL_WINDOWEVENT_FOCUS_LOST
+              ))
         {
-          //SDL_RenderSetLogicalSize(renderer, logical_width, logical_height);
-          //SDL_RenderSetScale(renderer, spooky_gui_get_scale_factor(), spooky_gui_get_scale_factor());
-          //if(SDL_GetWindowFlags(window) & SDL_WINDOW_MAXIMIZED) { spooky_gui_reset_scale_factor(renderer); }
+          SDL_RenderSetLogicalSize(renderer, context->logical_width, context->logical_height);
+
+          SDL_RenderSetScale(renderer, context->scale_factor_x, context->scale_factor_y);
+          if(SDL_GetWindowFlags(window) & SDL_WINDOW_MAXIMIZED) {
+            context->scale_factor_x = context->scale_factor_y = 1.0f;
+          }
         }
 #elif __unix__
         if (evt.type == SDL_WINDOWEVENT && (
-            evt.window.event == SDL_WINDOWEVENT_RESIZED
-        ))
+              evt.window.event == SDL_WINDOWEVENT_RESIZED
+              ))
         {
-          //if(linux_resize) {
-            //spooky_gui_init_gui(win, renderer);
-   
-            //logical_width = (int)(((float)spooky_window_width / 2) * spooky_gui_get_scale_factor());
-            //logical_height = (int)(((float)spooky_window_height / 2) * spooky_gui_get_scale_factor());
+#if 1==0
+          if(linux_resize) {
+            spooky_gui_init_gui(win, renderer);
 
-            //SDL_RenderSetLogicalSize(renderer, logical_width, logical_height);
-            //SDL_RenderSetScale(renderer, spooky_gui_get_scale_factor(), spooky_gui_get_scale_factor());
-          //}
-          //linux_resize = true;
+            logical_width = (int)(((float)spooky_window_width / 2) * spooky_gui_get_scale_factor());
+            logical_height = (int)(((float)spooky_window_height / 2) * spooky_gui_get_scale_factor());
+
+            SDL_RenderSetLogicalSize(renderer, logical_width, logical_height);
+            SDL_RenderSetScale(renderer, spooky_gui_get_scale_factor(), spooky_gui_get_scale_factor());
+          }
+          linux_resize = true;
+#endif
         }
 #endif
 
@@ -278,7 +302,7 @@ errno_t spooky_loop(const sp_game_context * context) {
 
       last_update_time += TIME_BETWEEN_UPDATES;
       update_loops++;
-    } /* while ((now - last_update_time ... */
+    } /* >> while ((now - last_update_time ... */
 
     if (now - last_update_time > TIME_BETWEEN_UPDATES) {
       last_update_time = now - TIME_BETWEEN_UPDATES;
@@ -287,8 +311,22 @@ errno_t spooky_loop(const sp_game_context * context) {
     //float interpolation = spooky_float_min(1.0f, (float)(now - last_update_time) / (float)(TIME_BETWEEN_UPDATES));
     //(void)interpolation;
     uint64_t this_second = (uint64_t)(last_update_time / BILLION);
- 
-    //SDL_SetRenderDrawColor(renderer, c0.r, c0.g, c0.b, c0.a);
+
+    SDL_Color c0;
+    SDL_GetRenderDrawColor(renderer, &c0.r, &c0.g, &c0.b, & c0.a);
+    {
+      SDL_Color bg = { .r = 20, .g = 1, .b = 36, .a = 255 };
+      SDL_SetRenderDrawColor(renderer, bg.r, bg.g, bg.b, bg.a);
+      SDL_RenderClear(renderer); /* letterbox color */
+
+      SDL_Color c = { .r = 1, .g = 20, .b = 36, .a = 255 };
+      SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a);
+      SDL_RenderFillRect(renderer, NULL); /* screen color */
+    }
+
+    SDL_RenderCopy(renderer, background, NULL, NULL);
+
+    SDL_SetRenderDrawColor(renderer, c0.r, c0.g, c0.b, c0.a);
     SDL_RenderPresent(renderer);
 
     last_render_time = now;
@@ -300,20 +338,6 @@ errno_t spooky_loop(const sp_game_context * context) {
 
       int mouse_x, mouse_y;
       SDL_GetMouseState(&mouse_x, &mouse_y);
-
-      /* Make the mouse 'sticky' in our window for 2 seconds: TODO: Doesn't work */
-      if (seconds_since_start % 2 == 0) {
-        // Every two seconds, see if the mouse needs to exit the window:
-        int x, y, w, h;
-        
-        SDL_GetWindowSize(window, &w, &h);
-        SDL_GetWindowPosition(window, &x, &y);
-        if ((mouse_x == last_x && (last_x <= 2 || last_x >= w - 2)) || (mouse_y == last_y && (last_y <= 2 || last_y >= h - 2))) {
-          //SDL_SetRelativeMouseMode(SDL_FALSE);
-          SDL_PumpEvents();
-          SDL_FlushEvent(SDL_WINDOWEVENT_LEAVE);
-        }
-      }
 
       last_x = mouse_x;
       last_y = mouse_y;
@@ -335,5 +359,13 @@ errno_t spooky_loop(const sp_game_context * context) {
 end_of_running_loop: ;
   } /* >> while(running) */
 
+  if(background != NULL) {
+    SDL_DestroyTexture(background), background = NULL;
+  }
+
   return SP_SUCCESS;
+
+err0:
+  return SP_FAILURE;
 }
+
