201e1d1cc44d9272da5b35b8e1b6e80876efdc75
Jason Short <53501+jgshort@users.noreply.github.com>
2020-07-31 16:06:58 -0700

    make str a linked list for fast hash access

--- src/sp_hash.c
+++ src/sp_hash.c
@@ -422,6 +422,8 @@ static spooky_str * spooky_hash_order_bucket_atoms(const spooky_hash_bucket * bu
       spooky_str * left = &bucket->atoms[bucket->atoms_limits.len - 2];
       spooky_str * right = &bucket->atoms[bucket->atoms_limits.len - 1];
       while(left >= bucket->atoms && right->hash < left->hash) {
+        left->next = right;
+        right->prev = left;
         if(atom == right) {
           spooky_str_swap(&left, &right);
           /* restore atom */
@@ -451,7 +453,7 @@ static spooky_str * spooky_hash_atom_alloc(const spooky_hash_table * self, spook
     *out_len = s_len;
   }
 
-  spooky_str_ref(s_cp, s_len, bucket->atoms_limits.len, hash, atom);
+  spooky_str_ref(s_cp, s_len, hash, atom);
   spooky_str_inc_ref_count(atom);
   bucket->atoms_limits.len++;
   atom = spooky_hash_order_bucket_atoms(bucket, atom);
@@ -606,8 +608,8 @@ char * spooky_hash_print_stats(const spooky_hash_table * self) {
             if(outer != inner && outer->hash == inner->hash) {
               collisions++;
               out += snprintf(out, max_buf_len - (size_t)(out - result), "Hash Collisions!\n");
-              out += snprintf(out, max_buf_len - (size_t)(out - result), "    X: '%s': len: %lu, hash: %lu, ordinal: %lu\n", outer->str, outer->len, outer->hash, outer->ordinal);
-              out += snprintf(out, max_buf_len - (size_t)(out - result), "    Y: '%s': len: %lu, hash: %lu, ordinal: %lu\n", inner->str, inner->len, inner->hash, inner->ordinal);
+              out += snprintf(out, max_buf_len - (size_t)(out - result), "    X: '%s': len: %lu, hash: %lu\n", outer->str, outer->len, outer->hash);
+              out += snprintf(out, max_buf_len - (size_t)(out - result), "    Y: '%s': len: %lu, hash: %lu\n", inner->str, inner->len, inner->hash);
             }
           }
         }
