5378b49e9d9e392df566a9cb548d16e691ac5006
Jason Short <53501+jgshort@users.noreply.github.com>
2020-07-31 17:11:32 -0700

    all of the buffoonery in a single commit

--- include/sp_str.h
+++ include/sp_str.h
@@ -22,6 +22,7 @@ void spooky_str_quit();
 
 void spooky_str_copy(spooky_str ** /* dest */, const spooky_str * /* src */);
 void spooky_str_swap(spooky_str ** /* left */, spooky_str ** /* right */);
+void spooky_str_list_swap(spooky_str * /* left */, spooky_str * /* right */);
 
 unsigned long spooky_hash_str(const char * restrict /* s */, size_t /* s_len */);
 errno_t spooky_str_ref(const char * /* s */, size_t /* len */, unsigned long /* hash */, spooky_str * /* out_str */);
