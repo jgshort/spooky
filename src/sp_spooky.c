5f9c43c42ce37f88fedfe84e9f8dc488f5d41839
Jason Short <53501+jgshort@users.noreply.github.com>
2021-06-29 08:16:41 -0700

    improve depth draw rendering

--- src/sp_spooky.c
+++ src/sp_spooky.c
@@ -976,7 +976,7 @@ static void spooky_tile_color(spooky_tile_type type, SDL_Color * color) {
   if(!color) { return; }
   switch(type) {
     case STT_EMPTY:
-      *color = (SDL_Color){ .r = 0, .g = 0, .b = 0, .a = 255 }; /* Black */
+      *color = (SDL_Color){ .r = 10, .g = 10, .b = 10, .a = 255 }; /* Black */
       break;
     case STT_BEDROCK:
       *color = (SDL_Color){ .r = 64, .g = 64, .b = 64, .a = 255 }; /* Dark gray */
@@ -1029,18 +1029,22 @@ static void spooky_render_landscape(SDL_Renderer * renderer, const spooky_contex
 
   for(size_t x = 0; x < MAX_TILES_ROW_LEN; x++) {
     for(size_t y = 0; y < MAX_TILES_COL_LEN; y++) {
-      size_t offset = SP_OFFSET(x, y, cursor->z);
-      spooky_tile_type type = tiles[offset].type;
       SDL_Color block_color = { 0 };
-      if(type == STT_EMPTY && cursor->z > 0) {
-        offset = SP_OFFSET(x, y, cursor->z - 1);
-        type = tiles[offset].type;
+      {
+        size_t offset = SP_OFFSET(x, y, cursor->z);
+        spooky_tile_type type = tiles[offset].type;
+        if(type == STT_EMPTY && cursor->z > 0) {
+          offset = SP_OFFSET(x, y, (cursor->z - 1));
+          type = tiles[offset].type;
+          spooky_tile_color(type, &block_color);
+          block_color.a -= 20;
+          block_color.a -= 20;
+          block_color.a -= 20;
+        } else {
+          spooky_tile_color(type, &block_color);
+        }
       }
 
-      spooky_tile_color(type, &block_color);
-      if(type == STT_EMPTY && cursor->z > 0) {
-        block_color.a = 200;
-      }
       const spooky_gui_rgba_context * tile_rgba = spooky_gui_push_draw_color(renderer, &block_color);
       {
         box->render(box, renderer);
